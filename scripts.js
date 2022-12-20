392ba4aa4d364b03aacab71a9d2e423fbed39988
JaelAguilar <cjaelaguilar@gmail.com>
2022-12-19 21:40:09 -0600

    Added encryption functionality

--- scripts.js
+++ scripts.js
@@ -7,10 +7,36 @@ let copyButton = document.querySelector('#copy-button');
 
 //Variable declaration
 let input;
-let keys = {
+const keys = {
     'e': 'enter',
     'i': 'imes',
     'a': 'ai',
     'o': 'ober',
     'u': 'ufat'
+}
+
+// Functions
+function encrypt(input) {
+    let length = input.length;
+    let result=''
+    for (i = 0; i < length; i++){
+        result += Object.keys(keys).includes(input[i]) ? keys[input[i]] : input[i];
+    }
+    resultMessage.innerText = result;
+}
+
+function isValid(input) {
+    if (input.toLowerCase() === input &&
+        input != '' &&
+        /[a-z .,:]+/.test(input)) {
+        return true;
+    }
+    return false;
+}
+// Asign functions
+encryptButton.onclick = () => {
+    input = userInput.value;
+    if (isValid(input)) {
+        encrypt(input)
+    }
 }
