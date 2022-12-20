85d5648156e2fc56e119293f0e471345bbd2a94a
JaelAguilar <cjaelaguilar@gmail.com>
2022-12-19 21:22:17 -0600

    Variable decalrations

---
+++ scripts.js
@@ -0,0 +1,16 @@
+// Obtain HTML Objects
+let encryptButton = document.querySelector('#encrypt-button');
+let decryptButton = document.querySelector('#decrypt-button');
+let userInput = document.querySelector('#user-input');
+let resultMessage = document.querySelector('#result-message');
+let copyButton = document.querySelector('#copy-button');
+
+//Variable declaration
+let input;
+let keys = {
+    'e': 'enter',
+    'i': 'imes',
+    'a': 'ai',
+    'o': 'ober',
+    'u': 'ufat'
+}
